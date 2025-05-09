d6d86136d382bbbb862ab6c8337e65811ffc9848
valenaruanno <valentinaruanno2004@gmail.com>
2025-05-09 09:08:59 -0300

    Agrego la carpeta frontend, meto el src en la carpeta backend para organizarla y agrego la conexion con la base de datos

---
+++ Backend/config/db.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Error de conexión a la base de datos:', err);
+  } else {
+    console.log('✅ Conexión exitosa a la base de datos MySQL');
+  }
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//import UserList from './components/UserList'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Alquileres de autos Maria</h1>
+       {/* <UserList /> */}
+    </div>
+  )
+}
+
+export default App
+
