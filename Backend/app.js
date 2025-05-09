3cfcb32c2aad6f619d6b32dfc1cc36324e294bc7
Diego <diegoph410@gmail.com>
2025-05-09 13:41:25 -0300

    Base para comunicacion backend-froentend

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const userRoutes = require('./routes/userRoutes');
 
-app.use(express.json()); //significa que acepta solicitudes con cuerpo en formato JSON (como los que envía Postman)
+// Middleware to parse JSON
+app.use(express.json());
+app.use(userRoutes); //ruta para el registro de usuarios
 app.get('/', (req, res) => { //ruta base para comprobar que el servidor esta corriendo
     res.send('Servidor corriendo'); //devuelve un mensaje de exito
 });
-app.use('/api/users', userRoutes); //cada vez que llegue una solicitud a la ruta /api/users se ejecutara el router de usuarios (delega)
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
 });
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const registerController = require('../controllers/user/registerController');
+const userController = require('../controllers/user/userController');
 
-router.post('/register', registerController.registerUser); //ruta para registrar un usuario
-router.get('/', registerController.listUsers); //ruta para listar los usuarios
+//router.post('/register', registerController.registerUser); //ruta para registrar un usuario
+router.get('/list', userController.listUsers); //ruta para listar los usuarios
 
 module.exports = router; //exporta el router para usarlo en otros archivos (app.js)
