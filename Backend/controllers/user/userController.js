08da949fba229fb0fb7817fbbc7fff7085d01434
fabioo66 <torrejonfabio@gmail.com>
2025-05-10 17:53:26 -0300

    Base para particion de ramas

--- Backend/controllers/user/userController.js
+++ Backend/controllers/user/userController.js
@@ -1,18 +1,2 @@
 const users = require('../../models/user'); //importa el array de la base de datos
 
-exports.listUsers = (req, res) => { //crea y exporta una arrow function para usarse en otros archivos (routes)
-  if (users.length === 0) { //verifica si la base de datos esta vacia
-    return res.status(404).json({ message: 'No hay usuarios registrados' }); //si no hay usuarios devuelve un error 404
-  }
-  return res.status(200).json(users); //devuelve la lista de usuarios
-}
-
-exports.createUser = async (req, res) => {
-  const { nombre, email } = req.body;
-  try {
-    const userId = await User.createUser(nombre, email);
-    res.status(201).json({ id: userId, nombre, email });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al crear usuario' });
-  }
-};
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,11 +1,2 @@
 const pool = require('../config/db');
 
-async function createUser(nombre, email) {
-  const [result] = await pool.query(
-    'INSERT INTO usuarios (nombre, email) VALUES (?, ?)',
-    [nombre, email]
-  );
-  return result.insertId;
-}
-
-module.exports = { createUser };
