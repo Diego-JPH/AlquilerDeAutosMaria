83f2a792175b31cab34ed4da4eb93ab65254d26b
Diego <diegoph410@gmail.com>
2025-05-15 16:42:31 -0300

    Merge de la rama Diego

--- Backend/app.js
+++ Backend/app.js
@@ -3,20 +3,14 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 const reserveRoutes = require('./routes/reserveRoutes'); //importa las rutas de reservas
 
+// Middleware to parse JSON
 app.use(express.json());
-<<<<<<< HEAD
 
 app.use('/api/reserve', reserveRoutes); //ruta para las reservas
-
+app.use('/api/user', userRoutes); //ruta para los usuarios
 app.get('/', (req, res) => { //ruta base para comprobar que el servidor esta corriendo
     res.send('Servidor corriendo'); //devuelve un mensaje de exito
 });
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
 });
-=======
-app.use('/api', userRoutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
->>>>>>> 06d1af7d5bf8410f5a9f9d427ae8d4e3508ea315
