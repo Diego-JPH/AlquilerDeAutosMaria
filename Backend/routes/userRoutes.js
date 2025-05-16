1312ed60c5d71e034326244f8e3de9dd7060d618
Valentin Capelli <valentin.capelli@hotmail.com>
2025-05-16 14:47:37 -0300

    Agrego la ruta para hacer hacer la funcionalidad de modificar info. de vehiculo

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/user/userController');
 
 //router.post('/register', registerController.registerUser); //ruta para registrar un usuario
-router.get('/list', userController.listUsers); //ruta para listar los usuarios
+//router.get('/list', userController.listUsers); //ruta para listar los usuarios
 
 module.exports = router; //exporta el router para usarlo en otros archivos (app.js)
