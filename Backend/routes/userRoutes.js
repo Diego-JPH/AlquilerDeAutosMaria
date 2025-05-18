9775cd9f3e90f4afd96bea13af5a4d80fe6d8058
valenaruanno <valentinaruanno2004@gmail.com>
2025-05-18 17:46:17 -0300

    Implementamos la funcionalidad de crear reserva. Falta corregir que el conductor no pueda reservar siendo menor de edad y que no pueda reservar un conductor con reserva activa

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const registerController = require('../controllers/user/registerController');
+
 const recoverController = require('../controllers/user/recoverPassword');
 
 router.post('/registrar-cliente', userController.registrarCliente);
