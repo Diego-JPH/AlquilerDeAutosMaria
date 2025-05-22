ea73cfeaa16e52f1526e822fd8c8ef7cffe4b14e
fabioo66 <torrejonfabio@gmail.com>
2025-05-22 19:21:22 -0300

    minor changes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const verificarToken = require('../middlewares/authMiddleware');
-const recoverController = require('../controllers/user/recoverPassword');
 
 router.post('/registrar-cliente', userController.registrarCliente);
 router.post('/login', userController.iniciarSesion);
-router.post('/recover-password', recoverController.recoverPassword);
 
 module.exports = router;
-
-
-
