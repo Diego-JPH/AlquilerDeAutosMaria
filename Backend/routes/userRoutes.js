62f09b2921ae93ad70567fcdfa3ed30ee7e3522b
fabioo66 <torrejonfabio@gmail.com>
2025-05-22 19:02:51 -0300

    Agregar ruta protegida para iniciar sesion en userRoutes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+const verificarToken = require('../middlewares/authMiddleware');
+
 router.post('/registrar-cliente', userController.registrarCliente);
 router.post('/login', userController.iniciarSesion);
 
