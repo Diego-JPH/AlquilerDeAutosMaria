6d97ebc2fcc90339fa6468ecc4595b7cc5718cf6
fabioo66 <torrejonfabio@gmail.com>
2025-05-22 19:03:13 -0300

    Actualizo app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const reserveRoutes = require('./routes/reserveRoutes'); //importa las rutas de reservas
+require('dotenv').config();
+
 // Middleware to parse JSON
 app.use(express.json());
-
 app.use('/api/reserve', reserveRoutes); //ruta para las reservas
 app.use('/api/user', userRoutes); //ruta para los usuarios
 app.get('/', (req, res) => { //ruta base para comprobar que el servidor esta corriendo
