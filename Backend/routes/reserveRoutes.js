2a0952c178fc9fc6c7cb03d9f119aac9f680c8cb
Diego <diegoph410@gmail.com>
2025-05-23 00:56:45 -0300

    Actualizacion de dependencias y RealizarReserva

--- Backend/routes/reserveRoutes.js
+++ Backend/routes/reserveRoutes.js
@@ -4,5 +4,6 @@ const reserveController = require('../controllers/reserveControllers');
 
 router.post('/cancel-reserve', reserveController.cancelReserve); //ruta para cancelar una reserva
 router.put('/change-driver', reserveController.changeDriver); //ruta para cambiar el conductor de una reserva
+router.post('/create-reserve', reserveController.reserveVehicle); //ruta para crear una reserva
 console.log('Cargando rutas de reservas...');
 module.exports = router; //exporta el router para usarlo en otros archivos (app.js)
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const registerController = require('../controllers/user/registerController');
 const recoverController = require('../controllers/user/recoverPassword');
 
 router.post('/registrar-cliente', userController.registrarCliente);
